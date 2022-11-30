fe39713e12edc017be54584eeef5e3bfb3e32375
dima <zhuikovda.gmail.com>
2022-11-30 20:47:22 +0300

    SantaClausableInterface

---
+++ 7kyu/SantaClausableInterface.js
@@ -0,0 +1,6 @@
+
+function isSantaClausable(obj) {
+    return ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'].every(function(methodName) {
+      return typeof obj[methodName] == 'function';
+    });
+  }
